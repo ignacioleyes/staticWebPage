a20b0413f3f9e688bb240e8944402c4e8c2f681d
Juan Ignacio Leyes Schneider <juanignacioleyes@gmail.com>
2024-05-22 20:20:17 -0300

    fix/(index.html): google tag manager error

---
+++ googleTagManager.js
@@ -0,0 +1,13 @@
+export function loadGoogleTagManager(containerId) {
+    const script = document.createElement("script");
+    script.async = true;
+    script.src = import.meta.env.VITE_GOOGLE_TAG_MANAGER_SOURCE;
+    document.head.appendChild(script);
+
+    window.dataLayer = window.dataLayer || [];
+    function gtag() {
+        dataLayer.push(arguments);
+    }
+    gtag("js", new Date());
+    gtag("config", containerId);
+}
