f97b6159fe809c7426adcdbf53e24aaf120e1f52
Juan Ignacio Leyes Schneider <juanignacioleyes@gmail.com>
2024-11-07 18:53:42 -0300

    rename tagManager function to analytics

--- analytics.js
+++ analytics.js
@@ -1,7 +1,7 @@
-export function loadGoogleTagManager(containerId) {
+export function loadAnalytics(containerId) {
     const script = document.createElement("script");
     script.async = true;
-    script.src = import.meta.env.VITE_CD_TAG_MANAGER_SOURCE;
+    script.src = import.meta.env.VITE_ANALYTICS_SOURCE;
     document.head.appendChild(script);
     window.dataLayer = window.dataLayer || [];
     function gtag() {
